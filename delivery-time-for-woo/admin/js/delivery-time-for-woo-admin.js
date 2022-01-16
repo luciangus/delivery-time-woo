a59acd2d409e3451272b2a9952f4594d1245775b
luciangus <50838440+luciangus@users.noreply.github.com>
2022-01-16 10:27:35 -0300

    Add files via upload

---
+++ delivery-time-for-woo/admin/js/delivery-time-for-woo-admin.js
@@ -0,0 +1,11 @@
+(function( $ ) {
+	'use strict';
+
+	
+	$(function() {
+		// Add Color Picker to all inputs that have 'dtf_color' class
+		$( '#dtf_color' ).wpColorPicker();
+	});
+	
+
+})( jQuery );
---
+++ delivery-time-for-woo/includes/class-delivery-time-for-woo-deactivator.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @since      1.0.0
+ *
+ * @package    Delivery_Time_For_Woo
+ * @subpackage Delivery_Time_For_Woo/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Delivery_Time_For_Woo
+ * @subpackage Delivery_Time_For_Woo/includes
+ */
+class Delivery_Time_For_Woo_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
